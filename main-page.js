ea6bae775adceaa058a9b9ed93b7030b8a65adbc
Geraint <luffgd@gmail.com>
2014-11-09 18:22:05 +0000

    Update demo page

--- main-page.js
+++ main-page.js
@@ -6,10 +6,11 @@ define(['caution'], function (caution) {
 		'marked': 'http://cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min.js',
 		'Prism': 'http://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js'		
 	});
-	caution.loadShim('marked', ['8208dd7d61227d3caeece575cfe01fcd60fce360fa7103abb0dc7f6329217eba']);
+
+	caution.load('marked', ['8208dd7d61227d3caeece575cfe01fcd60fce360fa7103abb0dc7f6329217eba']);
 	caution.loadShim('Prism', ['e904847187d6817a5f483b70c1d702703dd20d23bac7045968f5c889690d1a08']);
 	caution.get('http://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.css', ['b290c340249a46416d21ed64ff3a4162dc8a7c0e813654f23a5d36b65f72aab2'], function (error, css) {
-		if (error) return caution.missing('Prism CSS', []);
+		if (error) return caution.fail('Prism CSS', []);
 		var style = document.createElement('style');
 		css += 'pre[class*=language-]>code[data-language] {overflow: auto}';
 		style.innerText = style.innerHTML = css;
@@ -25,8 +26,10 @@ define(['caution', 'marked', 'Prism'], function (caution, marked, Prism) {
 		if (error) {
 			text = error.message + '\n\n```' + (error.stack || '(no stack)') + '```';
 		}
-		var html = marked(text, {gfm: true, sanitize: true});
-		document.body.innerHTML = html;
+		document.body.innerHTML = '<a id="bitbucket" href="https://bitbucket.org/geraintluff/caution.js">view on BitBucket</a>';
+		var div = document.createElement('div');
+		div.innerHTML = marked(text, {gfm: true, sanitize: true});
+		document.body.appendChild(div)
 		Prism.highlightAll();
 	});
 });
